83ce5414b55e6201c486d11b62148c8c1f1166c4
Miharitiana <miharitianarak@yahoo.com>
2023-02-07 12:37:45 +0300

    update

--- mysql.sql
+++ mysql.sql
@@ -48,15 +48,21 @@ insert into categories values(default,'Accessoires');
 insert into categories values(default,'Boky');
 insert into categories values(default,'High_tech');
 
-insert into objet values(default,1,1,,,);
-insert into objet values(default,2,2,,,);
-insert into objet values(default,3,3,,,);
-insert into objet values(default,1,4,,,);
-insert into objet values(default,2,1,,,);
-insert into objet values(default,3,2,,,);
-insert into objet values(default,1,3,,,);
-insert into objet values(default,2,4,,,);
-insert into objet values(default,3,1,,,);
+insert into objet values(default,1,1,'jean','pantalon à couture',150000);
+insert into objet values(default,2,2,'polo','vetement haut',100000);
+insert into objet values(default,3,3,'jogging','confortable et chaud',200000);
+
+insert into objet values(default,1,4,'lunettes','filtration de lumière',75000);
+insert into objet values(default,2,1,'casquette','à proteger la tête du soleil',50000);
+insert into objet values(default,3,2,'montre','dispositif mécanique',200000);
+
+insert into objet values(default,1,3,'harry_potter','',);
+insert into objet values(default,2,4,'Maigret','',);
+insert into objet values(default,3,1,'dune','',);
+
+insert into objet values(default,3,1,'ordinateur','',);
+insert into objet values(default,3,1,'souris','',);
+insert into objet values(default,3,1,'Unité_central','',);
 
 insert into photoObj values();
 insert into photoObj values();
